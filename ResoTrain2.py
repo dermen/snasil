c7d257f04e7a3ea7d52a53fc02d5e2016f66b8bc
Wesley Goh <goh.wesley8921@gmail.com>
2024-07-16 17:31:25 -0700

    Adding reso train script

---
+++ ResoTrain2.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+#Test comment
+
+import h5py
+import pylab as plt
+
+
+# In[9]:
+
+
+h = h5py.File("test_data/compressed12.h5","r")
+
+
+# In[10]:
+
+
+i_img = 0
+
+
+# In[11]:
+
+
+labels = list(h['labels'].attrs['names'])
+
+
+# In[12]:
+
+
+labels
+
+
+# In[13]:
+
+
+ridx = labels.index("reso")
+
+
+# In[14]:
+
+
+r = h['labels'][i_img, ridx]
+
+
+# In[15]:
+
+
+xidx = labels.index("cent_fast_train")
+
+
+# In[16]:
+
+
+yidx = labels.index("cent_slow_train")
+
+
+# In[17]:
+
+
+x,y = h['labels'][i_img,[xidx,yidx]]
+
+
+# In[18]:
+
+
+plt.imshow(h['images'][i_img,[xidx,yidx]])
+
+
+# In[19]:
+
+
+plt.plot([x],[y],'rx',ms=10)
+
+
+# In[20]:
+
+
+plt.imshow(h['images'][i_img], vmax = 20, cmap='gray_r')
+
+
+# In[21]:
+
+
+plt.plot([x],[y],'rx',ms=10)
+
+
+# In[22]:
+
+
+plt.title(f"res={r:.2f}, cent={x:.2f},{y:.2f}")
+
+
+# In[23]:
+
+
+plt.show()
+
+
+# In[24]:
+
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import h5py
+import torchvision.transforms as transforms
+import numpy as py
+import os
+import pylab as plt
+print (torch.cuda.is_available())
+print("Found %d devices" %torch.cuda.device_count())
+
+
+# In[25]:
+
+
+i_img = 0
+labels = list(h['labels'].attrs['names'])
+
+
+# In[26]:
+
+
+class wTest:
+    def __init__(self, h5_file_path, label_name):
+        self.file = h5py.File(file_path, 'r')
+        self.labels_names = list(self.file["labels"].attrs["names"])
+        print (self.labels_names, len(self.labels_names))
+        self.labels = self.file['labels']
+        self.images = self.file['images']
+        self.label_name = label_name # defined label_name here
+        print (self.labels)
+        
+    def __len__(self):
+        return len(self.labels)
+        
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError("out of range")
+        img = self.images[index]
+        img = img.reshape(1, 820, 820)  # Add channel dimension
+        img = torch.tensor(img, dtype=torch.float32)  # Convert to tensor
+        lab_index = self.get_labels_index(self.label_name) # find index
+        assert isinstance(lab_index, list)
+        lab = torch.tensor([self.labels[index, i] for i in lab_index], dtype=torch.float32)
+        #else:
+        #lab = torch.tensor(self.labels[index, lab_index], dtype=torch.float32)
+        return img, lab
+        
+    def get_labels_index(self, label_name):
+        if isinstance(label_name, list):
+            return [self.labels_names.index(name) for name in label_name]
+        else:
+            return [self.labels_names.index(label_name)]
+            
+    def get_data_by_label(self, i_img, label_name):
+        label_index = self.get_labels_index(label_name)
+        return self.file['labels'][i_img, label_index][0]
+        
+    def get_xy_data(self, i_img, label_x, label_y):
+        x_idx = self.get_labels_index(label_x)
+        y_idx = self.get_labels_index(label_y)
+        x = self.file['labels'][i_img, x_idx]
+        y = self.file['labels'][i_img, y_idx]
+        return x[0], y[0]
+        
+    def plot_image(self, i_img, label_x, label_y, label_r, vmax=20, cmap='gray_r'):
+        r = self.get_data_by_label(i_img, label_r)
+        x, y = self.get_xy_data(i_img, label_x, label_y)
+        plt.imshow(self.file['images'][i_img], vmax=vmax, cmap=cmap)
+
+        plt.plot(x, y, 'rx', ms = 10)
+        plt.title(f"res={r:.2f}, cent={x:.2f},{y:.2f}")
+        plt.show()
+
+
+# In[27]:
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 202 * 202, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, input):
+        c1 = F.relu(self.conv1(input))
+        s2 = F.max_pool2d(c1, (2, 2))
+        c3 = F.relu(self.conv2(s2))
+        s4 = F.max_pool2d(c3, 2)
+        s4 = torch.flatten(s4, 1)
+        f5 = F.relu(self.fc1(s4))
+        f6 = F.relu(self.fc2(f5))
+        output = self.fc3(f6)
+        return output
+
+# Create a model instance
+net = Net()
+print(net)
+
+# Test with a single image tensor of shape (1, 1, 820, 820)
+input_tensor_single = torch.randn(1, 1, 820, 820)
+output_single = net(input_tensor_single)
+print(output_single.shape)  # Should print torch.Size([1, 1])
+
+# Test with a batch of 10 images tensor of shape (10, 1, 820, 820)
+input_tensor_batch = torch.randn(10, 1, 820, 820)
+output_batch = net(input_tensor_batch)
+print(output_batch.shape)  # Should print torch.Size([10, 1])
+
+
+# In[28]:
+
+
+file_path = "test_data/compressed12.h5"
+label_name = "reso"
+hdf5_dataset = wTest(file_path, label_name)
+dataloader = DataLoader(hdf5_dataset, batch_size=10, shuffle=True)
+
+
+# In[29]:
+
+
+label_r = "reso"
+label_x = "cent_fast_train"
+label_y = "cent_slow_train"
+i_img = 0
+
+
+# In[30]:
+
+
+from torch.utils.data import DataLoader
+
+reso_dataloader = DataLoader(hdf5_dataset, batch_size=64, shuffle=True)
+label_r = "reso"
+
+
+# In[31]:
+
+
+hdf5_dataset.plot_image(i_img, label_x, label_y, label_r)
+
+
+# In[32]:
+
+
+train_features, train_labels = next(iter(reso_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img ,cmap='gray_r', vmin=0, vmax=20)
+plt.show()
+print(f"Label: {label}")
+
+
+# In[33]:
+
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+# In[34]:
+
+
+net = Net()
+net.train()
+criterion = nn.MSELoss()  
+optimizer = torch.optim.SGD(net.parameters(), lr=0.00001, momentum=0.9) #optim.adder
+
+# Test the model with a sample tensor
+sample_input = torch.randn(1, 1, 820, 820) 
+output = net(sample_input)
+
+
+print("Output shape:", output.shape)
+print("Output:", output)
+
+
+total_loss = 0.0
+for i, data in enumerate(dataloader, 0):
+    inputs, labels = data
+    
+    optimizer.zero_grad()
+    outputs = net(inputs)
+
+    #from IPython import embed
+    #embed()
+    
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    
+    total_loss += loss.item()
+    print(f'Batch {i+1}, Loss: {loss.item()}')
+numBatches = len(dataloader)
+total_loss = total_loss / numBatches
+
+print('Finished Training %d' % numBatches)
+print('Total loss after 1 epoch:', total_loss)
+
+sample_input = torch.randn(1, 1, 820, 820)
+output = net(sample_input)
+print("Output shape:", output.shape)
+print("Output:", output)
+
+
+# In[45]:
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Training script for image dataset.')
+    parser.add_argument('--train_h5', type=str, required=True, help='Path to the training HDF5 file.')
+    parser.add_argument('--val_h5', type=str, required=True, help='Path to the validation HDF5 file.')
+    parser.add_argument('--label_name', type=str, required=True, help='Name of the label.')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train.')
+    parser.add_argument('--batch_size', type=int, default=10, help='Batch size for training.')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
+    return parser.parse_args()
+
+import torch.optim as optim
+from torch.nn.utils import clip_grad_norm_
+
+class Args:
+    train_h5 = 'path_to_train_h5_file.h5' 
+    val_h5 = 'path_to_val_h5_file.h5' 
+    label_name = ['one_over_reso']
+    epochs = 10
+    batch_size = 10
+    lr = 0.0001
+
+args = Args()
+
+print(f"Training settings: {args}")
+
+# Error handling for file paths
+try:
+    # Datasets and dataloaders
+    print("Loading datasets")
+    train_dataset = wTest(args.train_h5, args.label_name)
+    val_dataset = wTest(args.val_h5, args.label_name)
+    print("Datasets loaded successfully")
+except FileNotFoundError as e:
+    print(f"File not found: {e}")
+    raise
+except ValueError as e:
+    print(f"Value error: {e}")
+    raise
+
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+
+# Model, loss function, optimizer
+net = Net()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.09)
+verbose = False
+
+def plot_losses(epochs, train_losses, val_losses):
+    plt.plot(epochs, train_losses, label='Training Loss')
+    plt.plot(epochs, val_losses, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss over Epochs')
+    plt.legend()
+    plt.show()
+
+print("Starting training")
+
+train_losses = []
+val_losses = []
+
+for epoch in range(args.epochs):
+    net.train()
+    train_loss = 0.0
+    
+    for train_imgs, train_labs in train_loader:
+        optimizer.zero_grad()
+        outputs = net(train_imgs)
+        loss_all = criterion(outputs, train_labs)
+        loss = criterion(outputs, train_labs)
+        loss.backward()
+        clip_grad_norm_(net.parameters(), 1e6)
+        optimizer.step()
+        loss_i = loss.item()
+        train_loss += loss_i
+        if verbose:
+            print(loss_all)
+    train_loss /= len(train_loader)
+
+    net.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for val_imgs, val_labs in val_loader:
+            outputs = net(val_imgs)
+            loss = criterion(outputs, val_labs)
+            val_loss += loss.mean().item()
+    val_loss /= len(val_loader)
+    
+    train_losses.append(train_loss)
+    val_losses.append(val_loss)
+
+
+    print(f"Epoch {epoch+1} done. train_loss: {train_loss:.4f}, val_loss: {val_loss:.4f}")
+
+print("Training completed")
+
+epochs = range(1, args.epochs + 1)
+plot_losses(epochs, train_losses, val_losses)
+
+
+# In[ ]:
+
+
+
+
