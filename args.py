a9f3cb6047e44b5ccc8b241e116509817e665e29
Ariana Amiri <amiriariana05@gmail.com>
2024-07-25 15:09:06 -0700

    add args.py

---
+++ args.py
@@ -0,0 +1,12 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Training script for image dataset.')
+    parser.add_argument('--train_h5', type=str, required=True, help='Path to the training HDF5 file.')
+    parser.add_argument('--val_h5', type=str, required=True, help='Path to the validation HDF5 file.')
+    parser.add_argument('--label_name', type=str, nargs = '+', required=True, help='Name of the label.')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train.')
+    parser.add_argument('--batch_size', type=int, default=10, help='Batch size for training.')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
+    args = parser.parse_args()
+    return args
